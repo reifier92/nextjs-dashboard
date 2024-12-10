083ed210ca52f59b4e39e364ceb09516f59b288e
reifier92 <reifier@kakao.com>
2024-12-10 18:10:41 +0900

    chore : react (& -dom) version upgrade 19.0.0

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,8 +1,8 @@
+import { Suspense } from 'react';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { Suspense } from 'react';
 import { 
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
